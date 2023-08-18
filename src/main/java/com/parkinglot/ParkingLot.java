d1e1cc1479e496dc00d4e7d0b6a64a8e8595a261
Jens Jovellano <jensjovellano@gmail.com>
2023-08-18 21:11:54 +0800

    refactor: added final to ticketCarMap and removed unnecessary functions

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -4,12 +4,10 @@ import com.parkinglot.exception.FullParkingLotException;
 import com.parkinglot.exception.UnrecognizedTicketException;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class ParkingLot {
-    private Map<ParkingTicket, Car> ticketCarMap = new HashMap<>();
+    private final Map<ParkingTicket, Car> ticketCarMap = new HashMap<>();
     private static final int capacity = 10;
 
     private boolean isFull() {
@@ -30,14 +28,6 @@ public class ParkingLot {
             throw new UnrecognizedTicketException();
         }
         return ticketCarMap.remove(parkingTicket);
-    }
-
-    public ParkingTicket getParkingTicketOfCar(Car car) {
-        List<Map.Entry<ParkingTicket, Car>> ticketWithMatchingCar = ticketCarMap.entrySet()
-                .stream()
-                .filter(entry -> entry.getValue().equals(car))
-                .collect(Collectors.toList());
-        return ticketWithMatchingCar.get(0).getKey();
     }
 
     public int getAvailableCapacity() {
