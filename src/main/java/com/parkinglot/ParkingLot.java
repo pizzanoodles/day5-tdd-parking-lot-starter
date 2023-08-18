d1a9806143632e51d62cd9a58a4bf09175c2036a
Jens Jovellano <jensjovellano@gmail.com>
2023-08-18 14:16:50 +0800

    refactor, feat: changed and added functions to implement a HashMap for Cars and ParkingTickets

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -1,9 +1,22 @@
 package com.parkinglot;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class ParkingLot {
+    private Map<Car, ParkingTicket> carsAndTickets = new HashMap<>();
     public ParkingTicket park(Car car) {
         ParkingTicket parkingTicket = new ParkingTicket();
-        car.setParkingTicket(parkingTicket);
+        carsAndTickets.put(car, parkingTicket);
         return parkingTicket;
+    }
+
+    public Car fetch(ParkingTicket parkingTicket) {
+        return new Car();
+    }
+
+    public ParkingTicket getParkingTicketOfCar(Car car) {
+        return carsAndTickets.get(car);
     }
 }
